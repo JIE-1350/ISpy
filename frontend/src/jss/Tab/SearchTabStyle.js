64d486fc7316bcad40e9b97634a7a425e614371e
hkod19 <hadiyakodvavi@gmail.com>
2022-01-22 17:20:19 -0500

    Update search bar UI
    -Change appstyle size to allow full screen to be covered
    -Change display mode to allow textbox in search bar to flow (instead of becoming hidden) when screen size is changed
    -add dropdown for search type and search by (for date range and number of days)
    -fix overall UI (increase space between textfeilds)

--- frontend/src/jss/Tab/SearchTabStyle.js
+++ frontend/src/jss/Tab/SearchTabStyle.js
@@ -1,22 +1,37 @@
+import { padding } from "@mui/system";
+
 const SearchTabStyle = () => {
     return {
         searchTab: {
             width: "100%",
-            display: "flex"
+            display: "flex",
+            paddingTop: "10px"
         },
         directoryWindow: {
             height: "100%",
-            width: "25%",
+            width: "20%",
         },
         mainWindow: {
             height: "100%",
-            width: "75%",
+            width: "80%",
+            justifyContent: 'flex-end',
+            flexWrap: 'wrap'
+        },
+        searchBar: {
+            display: "flex",
+            width: "100%",
+            paddingBottom: "10px",
+            flexWrap: 'wrap'
+        },
+        textFeild: {
+            width: "150px",
         },
         filterBar: {
             height: "50px",
             width: "100%",
             borderTop: "solid 1px gray",
-            borderBottom: "solid 1px gray"
+            borderBottom: "solid 1px gray",
+            paddingTop: "10px"
         }
     };
 };
--- frontend/src/main/Tab/SearchTab.js
+++ frontend/src/main/Tab/SearchTab.js
@@ -2,11 +2,13 @@ import React from "react";
 import {createUseStyles} from 'react-jss';
 import { connect } from "react-redux"
 
+import { display } from '@mui/system';
 import FilterForm from "./../components/FilterForm";
 import FilterBox from "./../components/FilterBox";
 import Table from "./../components/Table";
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
+import MenuItem from '@mui/material/MenuItem';
 
 import SearchTabStyle from './../../jss/Tab/SearchTabStyle.js';
 
@@ -20,10 +22,11 @@ const SearchTab = (props) => {
     const [user, setUser] = React.useState('')
     const [since, setSince] = React.useState('')
     const [until, setUntil] = React.useState('')
-    const [keyword, setKeyword] = React.useState('')
+    const [word, setWord] = React.useState('')
+    const [days, setDays] = React.useState('')
 
     const scrapeData = () => {
-        fetch('http://127.0.0.1:8000/?user=' + user + '&keyword=' + keyword + '&since=' + since + '&until=' + until)
+        fetch('http://127.0.0.1:8000/?user=' + user + '&word=' + word + '&days=' + days + '&since=' + since + '&until=' + until)
         .then((res)=>{
             return res.json();
         }).then((obj)=>{
@@ -43,15 +46,68 @@ const SearchTab = (props) => {
         })
     }
 
+    const searchType = [
+        {
+            value: 'Keyword',
+            label: 'Keyword'
+        },
+        {
+            value: 'Hashtag',
+            label: 'Hashtag'
+        }
+    ]
+
+    const timeRange = [
+        {
+            value: 'date_range',
+            label: 'Date Range'
+        },
+        {
+            value: 'days',
+            label: '# of Days'
+        }
+    ]
+    
+    const [search_type, setSearchType] = React.useState('Keyword');
+    const [time_range, setTimeRange] = React.useState('dateRange');
+
+    const handleSearchChange = (event) => {
+        setSearchType(event.target.value);
+    };
+
+    const handleTimeChange = (event) => {
+        setTimeRange(event.target.value);
+    };
+
     return (
     <div className={classes.searchTab}>
         <div className={classes.directoryWindow}> directoryWindow </div>
         <div className={classes.mainWindow}>
-            <TextField label="Username" variant="outlined" onChange={e => setUser(e.target.value)}/>
-            <TextField label="Start Date" variant="outlined" onChange={e => setSince(e.target.value)}/>
-            <TextField label="To Date" variant="outlined" onChange={e => setUntil(e.target.value)}/>
-            <TextField label="Keyword" variant="outlined" onChange={e => setKeyword(e.target.value)}/>
+            <div className={classes.searchBar}>
+            <div> 
+                <TextField select label="Search Type:" className={classes.textFeild} sx={{paddingRight: '5px', paddingBottom: '5px'}} onChange={handleSearchChange}>
+                    {searchType.map((option) => (
+                        <MenuItem key={option.value} value={option.value}>
+                        {option.label}
+                        </MenuItem>
+                    ))}
+                </TextField>
+            </div>
+            <TextField label= {search_type} className={classes.textFeild} variant="outlined" sx={{paddingRight: '5px', paddingBottom: '5px'}} onChange={e => setWord(e.target.value)}/>
+            <TextField label="Username" className={classes.textFeild} variant="outlined" sx={{paddingRight: '5px', paddingBottom: '5px'}} onChange={e => setUser(e.target.value)}/>
+            <div> 
+                <TextField select label="Search by:" className={classes.textFeild} sx={{paddingRight: '5px', paddingBottom: '5px'}} onChange={handleTimeChange}>
+                    {timeRange.map((option) => (
+                        <MenuItem key={option.value} value={option.value}>
+                        {option.label}
+                        </MenuItem>
+                    ))}
+                </TextField>
+            </div>
+            <TextField label="Start Date" className={classes.textFeild} variant="outlined" sx={{paddingRight: '5px', paddingBottom: '5px'}} onChange={e => setSince(e.target.value)}/>
+            <TextField label="To Date" className={classes.textFeild} variant="outlined" sx={{paddingRight: '5px', paddingBottom: '5px'}} onChange={e => setUntil(e.target.value)}/>
             <Button variant="contained" onClick={scrapeData}>Search</Button>
+            </div>
             <div className={classes.filterBar}>
                 {props.state === undefined ? '' : props.state.filters.map((filter, index) => (
                     <FilterBox index={index}></FilterBox>))
