0be19a1508bda2cd37a11a5459b73ee0d2dd5eb7
jcarpenter48 <ian.modelun@gmail.com>
2022-01-23 22:12:35 -0500

    Update FilterBox to add buttons representing added filters with on click removal of button and filters

--- frontend/src/jss/components/FilterBoxStyle.js
+++ frontend/src/jss/components/FilterBoxStyle.js
@@ -1,6 +1,17 @@
 const FilterBoxStyle = () => {
     return {
-
+	   Button: {
+		display: 'inline-block',
+		position: "relative"
+	  },
+	  ".relative":{
+		  "position": "relative",
+		  "backgroundColor": "#fff",
+	  },
+	  ".addButton":{
+		  position: "relative",
+		  display: 'inline-block'
+	  }
     };
 };
 
--- frontend/src/main/components/FilterBox.js
+++ frontend/src/main/components/FilterBox.js
@@ -2,17 +2,22 @@ import React from "react";
 import { connect } from "react-redux"
 import {createUseStyles} from 'react-jss';
 
+import Button from '@mui/material/Button';
+
 import FilterBoxStyle from './../../jss/components/FilterBoxStyle.js';
 const useStyles = createUseStyles(FilterBoxStyle)
 
 
 const FilterBox = (props) => {
     const classes = useStyles()
+	const [anchorEl, setAnchorEl] = React.useState(null);
     const {index} = props
 
-
-    const addFilter = (event) => {
-        fetch('http://127.0.0.1:8000/filter/remove?index=' + index)
+    const removeFilter = (event) => {
+        setAnchorEl(event.currentTarget);
+		event.currentTarget.remove();
+		let indexInt = parseInt(event.currentTarget.id);
+        fetch('http://127.0.0.1:8000/filter/remove?index=' + indexInt)
         .then((res)=>{
             return res.json();
         }).then((obj)=>{
@@ -27,15 +32,23 @@ const FilterBox = (props) => {
             else {
                 throw(JSON.stringify(obj))
             }
+			
         }).catch(e=>{
             alert(e);
         })
+		//location.reload(); //this can be used to bodge the issue with filter removal 
+		//hiding all later indexed filters from view until 'page' is reloaded
     };
 
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+		event.currentTarget.remove();
+    };
+	
     return (
-        <div>
-            {props.state === undefined ? '' : index + JSON.stringify(props.state.filters[index])}
-        </div>
+        <div id={index}>
+			<Button variant="contained" className={classes.addButton} id={index} onClick={removeFilter}>{props.state === undefined ? '' : index + JSON.stringify(props.state.filters[index])}</Button>
+		</div>
     );
 }
 
