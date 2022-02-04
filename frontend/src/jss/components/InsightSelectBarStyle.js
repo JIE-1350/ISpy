034245e77300ef16abbac6bdadad6f04b7249f5d
Thinh Pham <thinhpham2122@gmail.com>
2022-02-04 01:10:58 -0500

    Add style for selectInsightBar

--- frontend/src/jss/components/InsightSelectBarStyle.js
+++ frontend/src/jss/components/InsightSelectBarStyle.js
@@ -1,5 +1,14 @@
 const InsightSelectBarStyle = () => {
     return {
+        InsightSelectBar: {
+            display: "flex",
+            width: "100%",
+            paddingBottom: "10px",
+            flexWrap: 'wrap',
+            paddingTop: "10px",
+            paddingLeft: "5px",
+            borderBottom: "solid 1px gainsboro",
+        },
         textField: {
             width: "14%",
         },
