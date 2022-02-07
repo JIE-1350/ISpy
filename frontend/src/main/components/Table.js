be0e222200f2853e3c27ef4241b76524dca5ab9d
jcarpenter48 <ian.modelun@gmail.com>
2022-02-07 18:44:02 -0500

    WIP Table component

--- frontend/src/main/components/Table.js
+++ frontend/src/main/components/Table.js
@@ -1,12 +1,19 @@
 import React from "react";
 import { connect } from "react-redux"
 import { useEffect } from 'react'
+
 import { JsonToTable } from "react-json-to-table";
+
 import Box from '@mui/material/Box';
 import Fade from '@mui/material/Fade';
 import { CircularProgress } from '@mui/material';
 
+import {createUseStyles} from 'react-jss';
+import TableStyle from './../../jss/components/TableStyle.js';
+const useStyles = createUseStyles(TableStyle)
+
 const Table = (props) => {
+	const classes = useStyles()
     const searching = props.searching
     const dispatch = props.dispatch
 
@@ -38,40 +45,47 @@ const Table = (props) => {
         }, 1000)  // update table every 1000 milliseconds
         return () => clearInterval(interval)
     }, [searching, dispatch]);
-
-	const DisplayData=()=>{
-		return(
-		<div>
-			{props === undefined ? '' : (props.table.data).map(
-					([key, value])=>{
-						return(
-							<tr key={key}>
-							{Object.values(value).map((i) => (
-								<td>{i}</td>
-							))}
-							</tr>
-						)
-					}
-				)
-			}		
-		</div>
-		)
-	}
-		
+	
     return (
-        <div>
-		    <Box sx={{ height: 40 }}>
-				<Fade
-				  in={searching}
-				  style={{
-					transitionDelay: searching ? '800ms' : '0ms',
-				  }}
-				  unmountOnExit
-				>
-				  <CircularProgress />
-				</Fade>
-			</Box>	
-            <JsonToTable json={props.table.data}/>
+        <div style={{'overflow-x':'auto'}}>
+            <Box sx={{ height: 40 }}>
+                <Fade
+                in={searching}
+                  style={{
+                    transitionDelay: searching ? '800ms' : '0ms',
+                  }}
+                  unmountOnExit
+                >
+                 <CircularProgress />
+                </Fade>
+			</Box>
+			
+            <table class="tableContainer">
+                <thead>
+				    <tr>
+                    {props.table.data === undefined ? '' : Object.keys(props.table.data).map(
+						(key)=>{
+							return(
+								<td className={classes.tableCell}>{key}</td>
+							)
+						}
+					)}
+					</tr>
+                </thead>
+				<tbody>
+                    {props.table.data === undefined ? '' : Object.entries(props.table.data).map(
+						([idx, value])=>{
+							return(
+								<td className={classes.tableRow} key={idx}>
+								{Object.values(value).map((i) => (
+									<tr className={classes.tableCell}>{i}</tr>
+								))}
+								</td>
+							)
+						}
+					)}
+			     </tbody>
+            </table>
         </div>
     );
 }
