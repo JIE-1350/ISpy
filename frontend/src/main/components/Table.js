2b763b5fb137ef4181b4ba905a4ce65a0d917088
jcarpenter48 <ian.modelun@gmail.com>
2022-02-07 17:41:26 -0500

    Update Table.js with loading indicator and temporary table implementation using react-json-to-table

--- frontend/src/main/components/Table.js
+++ frontend/src/main/components/Table.js
@@ -1,7 +1,10 @@
 import React from "react";
 import { connect } from "react-redux"
 import { useEffect } from 'react'
-
+import { JsonToTable } from "react-json-to-table";
+import Box from '@mui/material/Box';
+import Fade from '@mui/material/Fade';
+import { CircularProgress } from '@mui/material';
 
 const Table = (props) => {
     const searching = props.searching
@@ -36,10 +39,39 @@ const Table = (props) => {
         return () => clearInterval(interval)
     }, [searching, dispatch]);
 
-
+	const DisplayData=()=>{
+		return(
+		<div>
+			{props === undefined ? '' : (props.table.data).map(
+					([key, value])=>{
+						return(
+							<tr key={key}>
+							{Object.values(value).map((i) => (
+								<td>{i}</td>
+							))}
+							</tr>
+						)
+					}
+				)
+			}		
+		</div>
+		)
+	}
+		
     return (
         <div>
-            {JSON.stringify(props)}
+		    <Box sx={{ height: 40 }}>
+				<Fade
+				  in={searching}
+				  style={{
+					transitionDelay: searching ? '800ms' : '0ms',
+				  }}
+				  unmountOnExit
+				>
+				  <CircularProgress />
+				</Fade>
+			</Box>	
+            <JsonToTable json={props.table.data}/>
         </div>
     );
 }
