20bcea27610b85cac6cc755dfe9173bde566e3df
jcarpenter48 <ian.modelun@gmail.com>
2022-02-09 18:42:03 -0500

    Formatting changes and missing Style file

---
+++ frontend/src/jss/components/TableStyle.js
@@ -0,0 +1,16 @@
+const TableStyle = () => {
+    return {
+        tableContainer: {
+            width: "100%",
+            display: "inline-flex",
+        },
+		tableCell: {
+			minHeight: "5px"
+		},
+		tableRow: {
+			minHeight: "5px"
+		}		
+    };
+};
+
+export default TableStyle;
--- frontend/src/main/components/Table.js
+++ frontend/src/main/components/Table.js
@@ -5,6 +5,8 @@ import { useRef } from 'react'
 
 // import { JsonToTable } from "react-json-to-table";
 
+// import MaterialTable from "material-table";
+
 import Box from '@mui/material/Box';
 import Fade from '@mui/material/Fade';
 import { CircularProgress } from '@mui/material';
@@ -14,33 +16,10 @@ import TableStyle from './../../jss/components/TableStyle.js';
 const useStyles = createUseStyles(TableStyle)
 
 const Table = (props) => {
-	const classes = useStyles()
+     const classes = useStyles()
     const searching = props.searching
     const dispatch = props.dispatch
-	const tableBodyRef = useRef(null);
-	
-	// this is a hacky solution
-	const transposeTable = (tbody, newContainerType = "tbody") => {
-	  const rows = Array.from(tbody.querySelectorAll("tr"))
-	  const newTbody = document.createElement(newContainerType)
 
-	  for (let rowIdx = 0; rowIdx < rows.length; rowIdx++) {
-		const row = rows[rowIdx]
-		const cells = Array.from(row.querySelectorAll("td, th"))
-
-		for (let cellIdx = 0; cellIdx < cells.length; cellIdx++ ) {
-		  const cell = cells[cellIdx]
-		  const newRow = newTbody.children[cellIdx] || document.createElement("tr")
-		  if (!newTbody.children[cellIdx]) {
-			newTbody.appendChild(newRow)
-		  }
-		  newRow.appendChild(cell.cloneNode(true))
-		}
-	  } 
-	  tbody.parentElement.appendChild(newTbody)
-	  tbody.parentElement.removeChild(tbody)
-	}
-	
     useEffect(() => {
         const updateTable = () => {
             fetch('http://127.0.0.1:8000/update-table')
@@ -70,8 +49,6 @@ const Table = (props) => {
         return () => clearInterval(interval)
     }, [searching, dispatch]);
 
-    // transposeTable(document.querySelector("tbody")); // currently errors at times because execution is not deferred
-	
     return (
         <div style={{'overflow-x':'auto'}}>
             <Box sx={{ height: 40 }}>
@@ -84,35 +61,35 @@ const Table = (props) => {
                 >
                  <CircularProgress />
                 </Fade>
-			</Box>
-			
+            </Box>
+       
             <table class="tableContainer">
                 <thead>
-				    <tr>
+                        <tr>
                     {props.table.data === undefined ? '' : Object.keys(props.table.data).map(
-						(key)=>{
-							return(
-								<th className={classes.tableCell}>{key}</th>
-							)
-						}
-					)}
-					</tr>
+                              (key)=>{
+                                   return(
+                                        <th>{key}</th>
+                                   )
+                              }
+                         )}
+                         </tr>
                 </thead>
-				<tbody ref={tableBodyRef}>
+                    <tbody>
                     {props.table.data === undefined ? '' : Object.entries(props.table.data).map(
-						([idx, value])=>{
-							return(
-								<tr className={classes.tableRow} key={idx}>
-								{Object.values(value).map((i) => (
-									<td className={classes.tableCell}>{i}</td>
-								))}
-								</tr>
-							)
-						}
-					)}
-			     </tbody>
+                              ([idx, value])=>{
+                                   return(
+                                        <tr>
+                                             {Object.values(value).map((d) => (
+                                             <td className={classes.tableCell}>{d}</td>
+                                             ))}
+                                        </tr>
+                                   )
+                              }
+                         )}
+                    </tbody>
             </table>
-			
+               
         </div>
     );
 }
