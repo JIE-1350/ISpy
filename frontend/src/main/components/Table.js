b3d43e332e5d88df65fbe4285c3e533a4562f45f
jcarpenter48 <ian.modelun@gmail.com>
2022-02-09 21:13:29 -0500

    Implement frontend table with material-table

--- frontend/src/main/components/Table.js
+++ frontend/src/main/components/Table.js
@@ -1,22 +1,21 @@
 import React from "react";
 import { connect } from "react-redux"
 import { useEffect } from 'react'
-import { useRef } from 'react'
 
 // import { JsonToTable } from "react-json-to-table";
 
-// import MaterialTable from "material-table";
+import MaterialTable from "material-table";
 
 import Box from '@mui/material/Box';
 import Fade from '@mui/material/Fade';
 import { CircularProgress } from '@mui/material';
 
-import {createUseStyles} from 'react-jss';
-import TableStyle from './../../jss/components/TableStyle.js';
-const useStyles = createUseStyles(TableStyle)
+// import {createUseStyles} from 'react-jss';
+// import TableStyle from './../../jss/components/TableStyle.js';
+// const useStyles = createUseStyles(TableStyle)
 
 const Table = (props) => {
-     const classes = useStyles()
+    // const classes = useStyles()
     const searching = props.searching
     const dispatch = props.dispatch
 
@@ -48,7 +47,7 @@ const Table = (props) => {
         }, 1000)  // update table every 1000 milliseconds
         return () => clearInterval(interval)
     }, [searching, dispatch]);
-
+	
     return (
         <div style={{'overflow-x':'auto'}}>
             <Box sx={{ height: 40 }}>
@@ -63,32 +62,13 @@ const Table = (props) => {
                 </Fade>
             </Box>
        
-            <table class="tableContainer">
-                <thead>
-                        <tr>
-                    {props.table.data === undefined ? '' : Object.keys(props.table.data).map(
-                              (key)=>{
-                                   return(
-                                        <th>{key}</th>
-                                   )
-                              }
-                         )}
-                         </tr>
-                </thead>
-                    <tbody>
-                    {props.table.data === undefined ? '' : Object.entries(props.table.data).map(
-                              ([idx, value])=>{
-                                   return(
-                                        <tr>
-                                             {Object.values(value).map((d) => (
-                                             <td className={classes.tableCell}>{d}</td>
-                                             ))}
-                                        </tr>
-                                   )
-                              }
-                         )}
-                    </tbody>
-            </table>
+			<div style={{ maxWidth: "100%" }}>
+			<MaterialTable
+			  title="Retrieved Tweets"
+			  columns={props.table.columns}
+			  data = {props.table.data}
+			/>
+			</div>
                
         </div>
     );
