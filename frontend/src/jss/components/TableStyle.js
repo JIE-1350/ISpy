ff568c8fefd4c26a1699ba23b509842fe6d54b50
Thinh Pham <thinhpham2122@gmail.com>
2022-02-16 21:37:27 -0500

    Refactor code

--- frontend/src/jss/components/TableStyle.js
+++ frontend/src/jss/components/TableStyle.js
@@ -1,18 +1,23 @@
 const TableStyle = () => {
     return {
-        headerStyle: {
-            backgroundColor: '#1976d2',
-            color: '#FFF',
-            padding: '10px',
-            borderRight: "0.5px solid white",
-            textAlign:'center'
-          },
-          cellStyle: {
-              padding: '10px',
-              textAlign:'center',
-              borderRight: "0.5px solid lightgrey",
-          },
-          columnsButton: true
+        tableContainer: {
+            overflowX: 'auto'
+        },
+        options: {
+            headerStyle: {
+                backgroundColor: '#1976d2',
+                color: '#FFF',
+                padding: '10px',
+                borderRight: "0.5px solid white",
+                textAlign: 'center'
+            },
+            cellStyle: {
+                padding: '10px',
+                textAlign: 'center',
+                borderRight: "0.5px solid lightgrey",
+            },
+            columnsButton: true
+        }
     };
 };
 
--- frontend/src/main/components/Table.js
+++ frontend/src/main/components/Table.js
@@ -2,8 +2,6 @@ import React from "react";
 import { connect } from "react-redux"
 import { useEffect } from 'react'
 
-// import { JsonToTable } from "react-json-to-table";
-
 import MaterialTable from "material-table";
 
 import tableIcons from "./MaterialTableIcons";
@@ -11,12 +9,12 @@ import Box from '@mui/material/Box';
 import Fade from '@mui/material/Fade';
 import { CircularProgress } from '@mui/material';
 
-// import {createUseStyles} from 'react-jss';
-// import TableStyle from './../../jss/components/TableStyle.js';
-// const useStyles = createUseStyles(TableStyle)
+import {createUseStyles} from 'react-jss';
+import TableStyle from './../../jss/components/TableStyle.js';
+const useStyles = createUseStyles(TableStyle)
 
 const Table = (props) => {
-    // const classes = useStyles()
+    const classes = useStyles()
     const searching = props.searching
     const dispatch = props.dispatch
 
@@ -50,7 +48,7 @@ const Table = (props) => {
     }, [searching, dispatch]);
 	
     return (
-        <div style={{'overflow-x':'auto'}}>
+        <div className={classes.tableContainer}>
             <Box sx={{ height: 40 }}>
                 <Fade
                 in={searching}
@@ -64,27 +62,27 @@ const Table = (props) => {
             </Box>
        
 			<div style={{ maxWidth: "100%" }}>
-			<MaterialTable 
-			  title="Retrieved Tweets"
-              icons={tableIcons}
-			  columns={props.table.columns}
-			  data = {props.table.data}
-              options={{
-                headerStyle: {
-                  backgroundColor: '#1976d2',
-                  color: '#FFF',
-                  padding: '10px',
-                  borderRight: "0.5px solid white",
-                  textAlign:'center'
-                },
-                cellStyle: {
-                    padding: '10px',
-                    textAlign:'center',
-                    borderRight: "0.5px solid lightgrey",
-                },
-                columnsButton: true
-              }}
-			/>
+			<MaterialTable
+                title="Retrieved Tweets"
+                icons={tableIcons}
+                columns={props.table.columns}
+                data={props.table.data}
+                options={{
+                    headerStyle: {
+                        backgroundColor: '#1976d2',
+                        color: '#FFF',
+                        padding: '10px',
+                        borderRight: "0.5px solid white",
+                        textAlign: 'center'
+                    },
+                    cellStyle: {
+                        padding: '10px',
+                        textAlign: 'center',
+                        borderRight: "0.5px solid lightgrey",
+                    },
+                    columnsButton: true
+                }}
+            />
 			</div>
                
         </div>
